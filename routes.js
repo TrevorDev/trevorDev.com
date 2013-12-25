7cbca39e7245d8c3a691f9d5fa48e3a719d4ebc5
Trevor <trevorjbaron@gmail.com>
2013-12-25 05:24:44 -0500

    added flash games

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ var home = rek('home.js');
 var generalPage = rek('generalPage.js');
 
 exports.init = function(app){
+	app.get('/page/flashDemo/:game', generalPage.showGame);
 	app.get('/page/:page', generalPage.show);
 	app.get('/*', home.show);
 }
--- views/generalPage.js
+++ views/generalPage.js
@@ -1,3 +1,10 @@
+exports.showGame = function(req, res){
+	res.template.game = req.params.game;
+	res.template.description = '';
+	res.template.page = 'demo';
+  res.render('flashDemo/index',res.template);
+};
+
 exports.show = function(req, res){
 	res.template.page = req.params.page;
   res.render(req.params.page+'/index',res.template);
